6d8b3d697f4f9d8c923361f567586d65bc0420c8
zkariae <zakariae.benakka1@gmail.com>
2026-05-05 01:18:58 +0200

    chore: clean main.c, remove log state machine test

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -3,52 +3,17 @@
 #include "systick.h"
 #include "log.h"
 
-typedef enum {
-    STATE_DEBUG = 0,
-    STATE_INFO,
-    STATE_WARN,
-    STATE_ERROR
-} LogState;
-
 int main(void)
 {
     systick_init();
     gpio_driver_init();
     uart_init();
 
-    uart_send_string("=== STM32F407 RFID RC522 ===\r\n");
-    uart_send_string("[INIT] System OK\r\n");
-
-    LogState state = STATE_DEBUG;
-    uint32_t counter = 0;
+    LOG_INFO("=== STM32F407 RFID RC522 ===");
+    LOG_INFO("System OK");
 
     while (1) {
-        switch (state) {
-            case STATE_DEBUG:
-                LOG_DEBUG("SysTick running");
-                LOG_DEBUG_INT("tick = ", systick_get_tick());
-                state = STATE_INFO;
-                break;
-
-            case STATE_INFO:
-                LOG_INFO("System nominal");
-                LOG_DEBUG_INT("counter = ", counter);
-                state = STATE_WARN;
-                break;
-
-            case STATE_WARN:
-                LOG_WARN("Voltage threshold approaching");
-                state = STATE_ERROR;
-                break;
-
-            case STATE_ERROR:
-                LOG_ERROR("Simulated error triggered");
-                counter++;
-                state = STATE_DEBUG;
-                break;
-        }
-
-        delay_ms(1000);
+        /* TODO: RFID RC522 driver */
     }
 
     return 0;
